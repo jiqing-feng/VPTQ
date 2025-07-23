bba918eec9f2304c89c8bf1b444273bd94e4f082
jiqing-feng <jiqing.feng@intel.com>
2025-07-23 15:02:04 +0000

    use index select to replace gather
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- vptq/ops/quant_gemm.py
+++ vptq/ops/quant_gemm.py
@@ -89,14 +89,10 @@ def dequant(
         if enable_residual:
             res_indices = res_indices.view(torch.uint16).to(torch.int64)
 
-    indices = indices.unsqueeze(-1).expand(-1, -1, -1, vector_len)
-    indices = indices.reshape(num_codebooks, -1, vector_len)
-
-    selected_centroids = torch.gather(centroids, 1, indices)
+    selected_centroids = centroids.index_select(1, indices.reshape(-1))
     selected_centroids = selected_centroids.view(
         num_codebooks, -1, group_size, vector_len
     )
-
     selected_centroids = selected_centroids.permute(0, 1, 3, 2)
     qweight = selected_centroids.reshape(num_codebooks, -1, group_size)
     qweight = qweight.permute(1, 0, 2)
@@ -106,10 +102,7 @@ def dequant(
         res_centroids = res_centroids.view(
             num_codebooks, num_res_centroids, vector_len
         )
-        res_indices = res_indices.unsqueeze(-1).expand(-1, -1, -1, vector_len)
-        res_indices = res_indices.reshape(num_codebooks, -1, vector_len)
-
-        selected_res_centroids = torch.gather(res_centroids, 1, res_indices)
+        selected_res_centroids = res_centroids.index_select(1, res_indices.reshape(-1))
         res_centroids = selected_res_centroids.reshape(
             num_codebooks, -1, group_size, vector_len
         )
@@ -127,17 +120,8 @@ def dequant(
         outlier_centroids = outlier_centroids.view(
             1, num_outlier_centroids, outlier_vector_len
         )
-
         outlier_indices = outlier_indices.view(torch.uint16).to(torch.int64)
-        outlier_indices = outlier_indices.unsqueeze(-1).expand(
-            -1, -1, -1, outlier_vector_len
-        )
-
-        outlier_indices = outlier_indices.reshape(1, -1, outlier_vector_len)
-        selected_outlier_centroids = torch.gather(
-            outlier_centroids, 1, outlier_indices
-        )
-
+        selected_outlier_centroids = outlier_centroids.index_select(1, outlier_indices.reshape(-1))
         outlier_centroids = selected_outlier_centroids.reshape(
             1, -1, outlier_size, outlier_vector_len
         )
